3299cbd804a20b378258b8150eb570d223169157
SofiaAugusta <sasutter518@gmail.com>
2023-10-19 19:33:25 -0500

    added hanger class (unfinished)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Hanger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Hanger.java
@@ -9,46 +9,38 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Base.Component;
-
 @Config
-public class ArmRotation implements Component {
+public class Hanger implements Component{
     private final DcMotor rotation;
-    public Slides arm;
-
     public double PULSES_PER_REVOLUTION;
     public int INIT;
-    public int FORWARD;
-    public int BACKWARD;
+    public int UP;
+    public int DOWN;
     public static int targetPosition = 0;
-    public boolean isTeleOp, forcePosition;
+    public boolean isTeleOp;
 
-    public double error, prevError = 0, time, prevTime = System.nanoTime() * 1e-9d, power;
-    public static double kP = 0.01, kD = 0, kG = 0;
+    public double error;
     Telemetry telemetry;
 
-    public ArmRotation(
-        String rotationName,
-        HardwareMap hardwareMap,
-        Telemetry telemetry,
-        boolean isTeleOp,
-        double init,
-        double forward,
-        double backward
-    ) {
+    public Hanger(
+            String rotationName,
+            HardwareMap hardwareMap,
+            Telemetry telemetry,
+            boolean isTeleOp,
+            double init,
+            double up,
+            double down
+    ){
         rotation = hardwareMap.get(DcMotor.class, rotationName);
 
         rotation.setDirection(DcMotorSimple.Direction.FORWARD);
 
         this.PULSES_PER_REVOLUTION = 2786.2;
         this.INIT = (int) (init * PULSES_PER_REVOLUTION);
-        this.FORWARD = (int) (forward * PULSES_PER_REVOLUTION);
-        this.BACKWARD = (int) (backward * PULSES_PER_REVOLUTION);
+        this.UP = (int) (up * PULSES_PER_REVOLUTION);
+        this.DOWN = (int) (down * PULSES_PER_REVOLUTION);
         this.isTeleOp = isTeleOp;
         this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-    }
-
-    public void addSlides(Slides arm) {
-        this.arm = arm;
     }
 
     @Override
@@ -64,25 +56,10 @@ public class ArmRotation implements Component {
 
     @Override
     public void update() {
-        update(forcePosition);
-    }
-
-    public void update(boolean forcePosition) {
         error = targetPosition - getCurrentPosition();
-        time = System.nanoTime() * 1e-9d;
-        this.forcePosition = forcePosition;
-//        if (forcePosition || arm.getCurrentPosition() > (2.731 * arm.PULSES_PER_REVOLUTION)) {
-            power = (kP * error) + (kD * -(error - prevError) / (time - prevTime)) + (kG * Math.cos(Math.toRadians(targetPosition * (PULSES_PER_REVOLUTION / 360))));
-//        }
-        if (forcePosition) {
-            power = 0;
-        }
-        if (!isBusy()) {
-            this.forcePosition = false;
-        }
-        setPower(power);
-        prevError = error;
-        prevTime = time;
+
+        //unfinished obviously
+
     }
 
     @Override
@@ -90,7 +67,6 @@ public class ArmRotation implements Component {
         telemetry.addData("RotationPosition", getCurrentPosition());
         telemetry.addData("RotationTarget", targetPosition);
         telemetry.addData("RotationError", error);
-        telemetry.addData("RotationPower", power);
         return null;
     }
 
@@ -99,45 +75,28 @@ public class ArmRotation implements Component {
     }
 
     public void toForward() {
-        move(FORWARD);
+        move(UP);
     }
 
     public void toBackward() {
-        move(BACKWARD);
-    }
-
-    public void toBackwardForce() {
-        update(true);
+        move(DOWN);
     }
 
     public void toggle() {
-        if (getCurrentPosition() + error == BACKWARD) {
+        if (getCurrentPosition() + error == DOWN) {
             toForward();
-        } else if (getCurrentPosition() + error == FORWARD) {
-            toBackward();
-        } else { // init position
+        } else if (getCurrentPosition() + error == UP) {
             toBackward();
         }
     }
 
     public void move(int position) {
-        if (arm.getCurrentPosition() > (2.731 * arm.PULSES_PER_REVOLUTION)) {
-            targetPosition = position;
-        }
-//            if (!isTeleOp) {
-//                while (isBusy()) {
-//                    update();
-//                }
-//            }
+        //unfinished
     }
 
     public void setPower(double motorPower) {
         if (motorPower > 1) motorPower = 1;
         rotation.setPower(motorPower);
-    }
-
-    public boolean isBusy() {
-        return Math.abs(error) > 10;
     }
 
     public int getCurrentPosition() {
