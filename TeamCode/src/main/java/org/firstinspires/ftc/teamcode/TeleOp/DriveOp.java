5e015a4b5d18444eb34a8beecceed4f11f352b31
ClumsyKitty0 <90661982+ClumsyKitty0@users.noreply.github.com>
2023-10-25 18:50:12 -0500

    made driveop exist and most likely work hopefully

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveOp.java
@@ -1,4 +1,61 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-public class DriveOp {
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Base.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Base.Robot;
+import org.firstinspires.ftc.teamcode.Bots.SirJohn;
+
+@TeleOp
+public class DriveOp extends BaseOpMode {
+    public SirJohn robot;
+    public double x, y, rot, speed;
+    public boolean slowmode;
+    @Override
+    protected Robot setRobot() {
+        this.robot = new SirJohn();
+        return null;
+    }
+
+    @Override
+    protected boolean setTeleOp() {
+        return true;
+    }
+    @Override
+    public void onInit() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    }
+    @Override
+    public void onStart() throws InterruptedException {
+        gamepadListener1.start.onRelease = () -> {
+            slowmode = !slowmode;
+        };
+    }
+
+    @Override
+    public void onUpdate() throws InterruptedException {
+
+        speed = (gamepad1.left_bumper ? 0.25 : (gamepad1.right_bumper || slowmode ? 0.5 : 1)) * (gamepad1.left_stick_button ? 1 : 0.65);
+        x = gamepad1.left_stick_x;
+        y = - gamepad1.left_stick_y;
+        rot = gamepad1.right_stick_x;
+
+        if (gamepad1.dpad_up) {
+            y = 1;
+        } else if (gamepad1.dpad_down) {
+            y = - 1;
+        }
+
+        if (gamepad1.dpad_right) {
+            x = 1;
+        } else if (gamepad1.dpad_left) {
+            x = - 1;
+        }
+
+        robot.mecanum.drive(x * speed, y * speed, rot * speed);
+        telemetry.update();
+
+    }
 }
