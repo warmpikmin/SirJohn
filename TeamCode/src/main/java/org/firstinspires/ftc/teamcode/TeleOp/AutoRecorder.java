ad233f5ff7fbb3e3dea157338f7c16484b6dd318
ClumsyKitty0 <90661982+ClumsyKitty0@users.noreply.github.com>
2023-10-30 18:47:03 -0500

    all error gone for realsies i hope

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/AutoRecorder.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
-
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-@TeleOp
-public class AutoRecorder extends MainOp {
-
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LineUp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LineUp.java
@@ -1,13 +1,13 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
-
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-@TeleOp
-public class LineUp extends MainOp {
-    @Override
-    public void onInit() throws InterruptedException {
-        super.onInit();
-        FtcDashboard.getInstance().startCameraStream(robot.camera.requestStart(), 0);
-    }
-}
+//package org.firstinspires.ftc.teamcode.TeleOp;
+//
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//@TeleOp
+//public class LineUp extends MainOp {
+//    @Override
+//    public void onInit() throws InterruptedException {
+//        super.onInit();
+//        FtcDashboard.getInstance().startCameraStream(robot.camera.requestStart(), 0);
+//    }
+//}
