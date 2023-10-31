752cf0e0f485677cc072013a26512ccfed072997
ClumsyKitty0 <90661982+ClumsyKitty0@users.noreply.github.com>
2023-10-31 17:02:11 -0500

    theoretically added vision for the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Camera.java
@@ -13,6 +13,13 @@ import org.firstinspires.ftc.teamcode.Base.Component;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 
@@ -28,6 +35,22 @@ public class Camera implements Component {
     public String deviceName;
     public Mecanum mecanum;
     private volatile ParkingPosition position = ParkingPosition.RIGHT;
+    private static Scalar
+        lowerRedBounds = new Scalar (175,0,0,255),
+        upperRedBounds = new Scalar (255,50,50,255),
+        lowerBlueBounds = new Scalar (0,0,175,255),
+        upperBlueBounds = new Scalar (50,50,255,255);
+
+    public Mat rightBlueMat = new Mat(),
+            rightRedMat = new Mat(),
+            leftBlurredMat = new Mat(),
+            leftBlueMat = new Mat(),
+            centerBlueMat = new Mat(),
+            leftRedMat = new Mat(),
+            centerRedMat = new Mat(),
+            centerBlurredMat = new Mat(),
+            rightBlurredMat = new Mat();
+
     public enum ParkingPosition {
         LEFT,
         CENTER,
@@ -108,6 +131,69 @@ public class Camera implements Component {
 
             }
         }
+    }
+    //TODO find out rectangle values
+    public Rect leftRect = new Rect(0,0,0,0);
+    public Rect rightRect = new Rect(0,0,0,0);
+    public Rect centerRect = new Rect(0,0,0,0);
+    public Mat processFrame(Mat input){
+        //TODO fix submat
+        Imgproc.blur(input, leftBlurredMat, new Size(5,5));
+        Imgproc.blur(input, centerBlurredMat, new Size(5,5));
+        Imgproc.blur(input, rightBlurredMat, new Size(5,5));
+        leftBlurredMat = leftBlurredMat.submat(leftRect);
+        rightBlurredMat = rightBlurredMat.submat(rightRect);
+        centerBlurredMat = centerBlurredMat.submat(centerRect);
+
+        Core.inRange(rightBlurredMat,lowerBlueBounds,upperBlueBounds,rightBlueMat);
+        Core.inRange(leftBlurredMat,lowerBlueBounds,upperBlueBounds, leftBlueMat);
+        Core.inRange(centerBlurredMat, lowerBlueBounds,upperBlueBounds,centerBlueMat);
+
+
+        Core.inRange(rightBlurredMat,lowerRedBounds,upperRedBounds,rightRedMat);
+        Core.inRange(leftBlurredMat,lowerRedBounds,upperRedBounds,leftRedMat);
+        Core.inRange(centerBlurredMat,lowerRedBounds,upperRedBounds,centerRedMat);
+
+        double centerBluePercent = Core.countNonZero(centerBlueMat);
+        double centerRedPercent = Core.countNonZero(centerRedMat);
+        double leftBluePercent = Core.countNonZero(leftBlueMat);
+        double leftRedPercent = Core.countNonZero(leftRedMat);
+        double rightBluePercent = Core.countNonZero(rightBlueMat);
+        double rightRedPercent = Core.countNonZero(centerRedMat);
+
+        boolean isBlue = Math.max(centerBluePercent,centerRedPercent) == centerBluePercent;
+
+        if(isBlue){
+            double maxBluePercent = Math.max(centerBluePercent,Math.max(leftBluePercent,rightBluePercent));
+            if(maxBluePercent == centerBluePercent){
+                position = ParkingPosition.CENTER;
+            } else if(maxBluePercent == leftBluePercent){
+                position = ParkingPosition.LEFT;
+            } else if(maxBluePercent == rightBluePercent){
+                position = ParkingPosition.RIGHT;
+            } else{
+                telemetry.addLine("does not see anything, but knows it is blue");
+            }
+        } else{
+            double maxRedPercent = Math.max(centerRedPercent, Math.max(leftRedPercent,rightRedPercent));
+            if(maxRedPercent == centerRedPercent){
+                position = ParkingPosition.CENTER;
+            } else if(maxRedPercent == leftRedPercent){
+                position = ParkingPosition.LEFT;
+            } else if(maxRedPercent == rightRedPercent){
+                position = ParkingPosition.RIGHT;
+            } else{
+                telemetry.addLine("does not see anything, but knows it is red");
+            }
+        }
+
+
+
+
+
+
+
+        return input;
     }
 
 }
